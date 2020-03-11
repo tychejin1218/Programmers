96b04b3704fbb7ee7296bb2b52c4d4d29c06081e
tychejin1218 <tychejin@naver.com>
2020-03-11 13:28:12 +0900

    [Programmers] 42583 : 다리를 지나는 트럭

--- Programmers/src/online/programmers/junit/ProblemStackQueue42583Test.java
+++ Programmers/src/online/programmers/junit/ProblemStackQueue42583Test.java
@@ -1,10 +1,13 @@
 package online.programmers.junit;
 
+import org.junit.Test;
+
 import online.programmers.problem.ProblemStackQueue42583;
 
 public class ProblemStackQueue42583Test {
 
-	public ProblemStackQueue42583Test() {
+	@Test
+	public void test() {
 
 		int answer = 0;
 		int bridge_length = 2;
@@ -13,7 +16,7 @@ public class ProblemStackQueue42583Test {
 
 		ProblemStackQueue42583 problemStackQueue42583 = new ProblemStackQueue42583();
 		answer = problemStackQueue42583.solution(bridge_length, weight, truck_weights);
-		
+
 		System.out.println("answer : " + answer);
 	}
 }
